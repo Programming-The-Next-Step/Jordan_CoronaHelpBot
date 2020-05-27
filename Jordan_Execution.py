1d8df7fcef711a573ec54b91030032d2c69d9b18
MarlaDre <marla.a.dressel@gmail.com>
2020-05-27 14:00:01 +0200

    Update Jordan_Execution.py

--- Jordan_Execution.py
+++ Jordan_Execution.py
@@ -4,12 +4,9 @@ Created on Tue May 26 15:30:07 2020
 
 @author: User
 """
+import Jordan_Class # import methods of the Jordan class
 
-import re
-import random
-from libraries import library_cursing
-import Jordan
-
+j = Jordan_Class()
 
 while True:
     print("Welcome. My name is Jordan. You can now choose a topic that we can talk about.")
@@ -17,21 +14,33 @@ while True:
     print("Press '2' to tell me about your deepest sorrows and your destroyed soul.")
     print("Press '3' to curse with me.")
     print("Press '4' to talk about Corona.")
+    print("Press '5' for a quick meditation.")
+    print("Press 'q' to quit.")
+    # execute this welcome text and tell user what to press in order to pick a topic.
     
-    choice = input(">>> ")
+    choice = input(">>> ") # initialize input
     
-    
-    if choice == '1' :
-        print("Alrighty, let's do some chitchatting.")
-        Jordan.smalltalk_Jordan()
-    if choice == '2' :
+    if choice == '1': # determine which key to press to initiate the specific topic
+        print("Alrighty, let's do some chitchatting.")# initiate welcome text for specific topic
+        print("Don't forget that I am sensitive to punctuation.")
+        j.smalltalk_Jordan() # initiate method
+    if choice == '2':
         print("Please tell me about your deepest sorrows and your destroyed soul.")
-        Jordan.caring_Jordan()   
-    if choice == '3' :
+        print("Don't forget that I am sensitive to punctuation.")
+        j.caring_Jordan()   
+    if choice == '3':
         print("Make yourself ready. let's insult each other!")
-        Jordan.cursing_Jordan()
-    if choice == '4' :
+        print("Don't forget that I am sensitive to punctuation.")
+        j.cursing_Jordan()
+    if choice == '4':
         print("Ok then, let's talk about Corona.")
-        Jordan.testing_Jordan()
-    else:
+        print("Don't forget that I am sensitive to punctuation..")
+        j.testing_Jordan()
+    if choice == '5':
+        print("I will be your guide in this meditation.")
+        print("Don't forget that I am sensitive to punctuation..")
+        j.testing_Jordan()
+    if choice == 'q': # if user wants to quit
         break
+    else: # if user pressed the wrong key.
+        print("Try again.")
