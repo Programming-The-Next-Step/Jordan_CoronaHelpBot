b3b2fc03696145d6a290de53aba3ecd55a0a21e9
MarlaDre <marla.a.dressel@gmail.com>
2020-05-27 22:52:17 +0200

    Update Jordan_Test.py

--- Jordan_Test.py
+++ Jordan_Test.py
@@ -6,16 +6,16 @@ Created on Tue May 26 12:18:17 2020
 """
 import re # import regular expressions for the appropriate access to the libraries
 import random # import random to pick a random answer for Jordan from the libraries
-from Jordan_Libraries import library_smalltalk # import libraries
+from Jordan_Libraries import library_cursing # import libraries
 
 
 
 def testing_Jordan(): 
-    keys_smalltalk = list(map(lambda x:re.compile(x[0], re.IGNORECASE), library_smalltalk)) 
+    keys_cursing = list(map(lambda x:re.compile(x[0], re.IGNORECASE), library_cursing)) 
     # list(map()) applies a function to all elements of a specified object, in this case the small-talk library
     # lambda makes sure that re.compile is applied in a certain way to all elements of the library without being case-sensitive
     # re.compile makes sure that the elemets are turned into objects that can be matched later to another item in the library    
-    values_smalltalk = list(map(lambda x:x[1],library_smalltalk))
+    values_cursing = list(map(lambda x:x[1],library_cursing))
     # same here, but here we pick the second argument in the list x[1], which entails Jordan's answers
     
     
@@ -23,13 +23,22 @@ def testing_Jordan():
         userInput = input(">>> ") # this allows for the user to type in keys
         resp = "I did not understand what you said. Also, I am sensitive to punctuation." # initalize response variable
         counter = 0  # initalize counter
-        while resp == "I did not understand what you said. Also, I am sensitive to punctuation." and counter < len(keys_smalltalk): # determine when to start my loop
-            for i in range(0, len(keys_smalltalk)): # loop through the indexed library
-                match = keys_smalltalk[i].match(userInput) # look if input of the user matches with one of the words in the library
+        while resp == "I did not understand what you said. Also, I am sensitive to punctuation." and counter < len(keys_cursing): # determine when to start my loop
+            for i in range(0, len(keys_cursing)): # loop through the indexed library
+                match = keys_cursing[i].match(userInput) # look if input of the user matches with one of the words in the library
                 if match:
-                    resp = random.choice(values_smalltalk[i]) # if there is a match, pick a random answer from x[1]
+                    word = keys_cursing[i].split(userInput)[1]  # We have to take the first element of this list
+                    resp = random.choice(values_cursing[i]) # if there is a match, pick a random answer from x[1]
                 counter += 1 # make sure that the counter is now + 1 so it does not write the initialized response from the beginning but continues with the loop
                              # if there is no match though, then it will write the initialized answer
+        
+        if userInput == 'q':
+            print(random.choice(values_cursing[i]))
+            print("---------------------------------")
+            print("Do you want to choose another topic? Pick below or press 'q' to quit for realsies.")
+            print("---------------------------------")
+            break
+        resp = resp.format(word)
         print(resp) # print Jordan's answer
 
 while True:
@@ -44,12 +53,12 @@ while True:
     # initialize input
     
     
-    if choice == '1' : # determine which key to press to initiate the specific topic
-        print("Alrighty, let's do some chitchatting.") # introduction text for specific topic
+    if choice == '3' : # determine which key to press to initiate the specific topic
+        print("Alrighty, let's insult each other.") # introduction text for specific topic
         print("Don't forget that I am sensitive to punctuation.")
         testing_Jordan() # initiate method
     elif choice == 'q': # if user wants to quit
-        print("Alrighty. Please choose another topic.")
+        print("Goodbye then.")
         break
     else: # if user pressed the wrong key.
         print("Try again.")
