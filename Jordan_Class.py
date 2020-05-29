7a3014bd056b796e76b71695b74fed6f466253cf
MarlaDre <marla.a.dressel@gmail.com>
2020-05-29 17:32:00 +0200

    Update Jordan_Class.py

--- Jordan_Class.py
+++ Jordan_Class.py
@@ -43,6 +43,8 @@ class Jordan:
         function that initiates small-talk with Jordan
     cursing_Jordan(userInput)
         function that initiates cursing with Jordan
+    meditating_Jordan(userInput)
+        function that initiates cursing with Jordan
     caring_Jordan(userInput)
         function that initiates a talk with Jordan about your sorrows
     corona_Jordan(userInput)    
@@ -51,160 +53,109 @@ class Jordan:
     
     def __init__(self): # this function is always called when a new object of the class is called
         """
-        Constructs the Jordan class. Takes no arguments.
+        Constructing topics for Jordan. Takes no arguments.
         
         """
+        self.library_keys = str('library' + self.i)
+        self.library_values = str('library' + self.i)
         
-        self.keys_smalltalk = list(map(lambda x:re.compile(x[0], re.IGNORECASE), library_smalltalk)) 
+        self.keys = list(map(lambda x:re.compile(x[0], re.IGNORECASE), self.library_keys)) 
             # list(map()) applies a function to all elements of a specified object, in this case the small-talk library
             # lambda makes sure that re.compile is applied in a certain way to all elements of the library without being case-sensitive
             # re.compile makes sure that the elemets are turned into objects that can be matched later to another item in the library
-        self.values_smalltalk = list(map(lambda x:x[1],library_smalltalk))
+        self.values = list(map(lambda x:x[1],self.library_values))
             # same here, but here we pick the second argument in the list x[1], which entails Jordan's answers
-        self.keys_caring = list(map(lambda x:re.compile(x[0], re.IGNORECASE), library_caring))
-        self.values_caring = list(map(lambda x:x[1],library_caring))
-        self.keys_cursing = list(map(lambda x:re.compile(x[0], re.IGNORECASE), library_cursing))
-        self.values_cursing = list(map(lambda x:x[1],library_cursing))
-        self.keys_corona = list(map(lambda x:re.compile(x[0], re.IGNORECASE), library_corona))
-        self.values_corona = list(map(lambda x:x[1],library_corona))
-        self.keys_meditating = list(map(lambda x:re.compile(x[0], re.IGNORECASE), library_meditating))
-        self.values_meditating = list(map(lambda x:x[1],library_meditating))
         
-    def smalltalk_Jordan(self):
-        """
+    def chooseTopic(self):             
         
-        A function for chitchatting with Jordan.
-        
-        """
-        while True:
-            userInput = input(">>> ") # allows for user to type in something
-            resp = "I did not understand what you said. Also, I am sensitive to punctuation." # initalize response string in case Jordan does not recognize the words
-            counter = 0  # initialize counter 
-            while resp == "I did not understand what you said. Also, I am sensitive to punctuation." and counter < len(self.keys_smalltalk):
-                for i in range(0, len(self.keys_smalltalk)):  # loop through the indexed library
-                    match = self.keys_smalltalk[i].match(userInput) # look if input of the user matches with one of the words in the library
-                    if match: 
-                        word = self.keys_smalltalk[i].split(userInput)[1]  # We have to take the first element of this list                        
-                        resp = random.choice(self.values_smalltalk[i]) # if there is a match, pick a random answer from x[1]
-                    counter += 1 # make sure that the counter is now + 1 so it does not write the initialized response from the beginning but continues with the loop.
-                                 # if there is no match though, then it will write the initialized answer
-            if userInput == 'q':
-                print(random.choice(self.values_smalltalk[i]))
-                print("---------------------------------")
-                print("Do you want to choose another topic? Pick below or press 'q' to quit for realsies.")
-                print("---------------------------------")
-                break                                
-            resp = resp.format(word) # replace if applicable      
-            print(resp) # print response
+            while True:
+                userInput = input(">>> ") # this allows for the user to type in keys
+                resp = "I did not understand what you said. Also, I am sensitive to punctuation." # initalize response variable
+                counter = 0  # initalize counter
+                while resp == "I did not understand what you said. Also, I am sensitive to punctuation." and counter < len(self.keys): # determine when to start my loop
+                    for i in range(0, len(self.keys)): # loop through the indexed library
+                        match = self.keys[i].match(userInput) # look if input of the user matches with one of the words in the library
+                        if match:
+                            word = self.keys[i].split(userInput)[1]  # We have to take the first element of this list
+                            resp = random.choice(self.values)[i] # if there is a match, pick a random answer from x[1]
+                        counter += 1 # make sure that the counter is now + 1 so it does not write the initialized response from the beginning but continues with the loop
+                                     # if there is no match though, then it will write the initialized answer
                 
-    def caring_Jordan(self):
-        """
-        
-        A function to talk with Jordan about your deepest sorrows
-        and your destroyed soul.
-        
-        """
-        
-        while True:
-            userInput = input(">>>")
-            resp = "I did not understand what you said. Also, I am sensitive to punctuation." # initalize this
-            counter = 0  # initalize this
-            while resp == "I did not understand what you said. Also, I am sensitive to punctuation." and counter < len(self.keys_caring):
-                for i in range(0, len(self.keys_caring)):
-                    match = self.keys_caring[i].match(userInput)
-                    if match:
-                        word = self.keys_caring[i].split(userInput)[1]  # We have to take the first element of this list
-                        resp = random.choice(self.values_caring[i])
-                    counter += 1
-            if userInput == 'q':
-                print(random.choice(self.values_caring[i]))
-                print("---------------------------------")
-                print("Do you want to choose another topic? Pick below or press 'q' to quit for realsies.")
-                print("---------------------------------")
-                break
-            resp = resp.format(word) # replace if applicable                   
-            print(resp)
-            
-    def cursing_Jordan(self):
-        """
-        
-        A function to insult Jordan and get insults back.
-        
-        """
-        while True:
-            userInput = input(">>> ")
-            resp = "I did not understand what you said. Also, I am sensitive to punctuation." # initalize this
-            counter = 0  # initalize this
-            while resp == "I did not understand what you said. Also, I am sensitive to punctuation." and counter < len(self.keys_cursing):
-                for i in range(0, len(self.keys_cursing)):
-                    match = self.keys_cursing[i].match(userInput)
-                    if match:
-                        word = self.keys_caring[i].split(userInput)[1]  # We have to take the first element of this list                        
-                        resp = random.choice(self.values_cursing[i])
-                    counter += 1
-            if userInput == 'q':
-                print(random.choice(self.values_cursing[i]))
-                print("---------------------------------")
-                print("Do you want to choose another topic? Pick below or press 'q' to quit for realsies.")
-                print("---------------------------------")
-                break                   
-            resp = resp.format(word) # replace if applicable                     
-            print(resp)
-                
-    def corona_Jordan(self):
-
-        """
-        
-        A function to talk with Jordan about the corona-crisis
-        and potentially get some suggestions on how to stay sane.
-        
-        """
-        while True:
-            userInput = input(">>> ")
-            resp = "I did not understand what you said. Also, I am sensitive to punctuation." # initalize this
-            counter = 0  # initalize this
-            while resp == "I did not understand what you said. Also, I am sensitive to punctuation." and counter < len(self.keys_corona):
-                for i in range(0, len(self.keys_corona)):
-                    match = self.keys_corona[i].match(userInput)
-                    if match:
-                        word = self.keys_caring[i].split(userInput)[1]  # We have to take the first element of this list                        
-                        resp = random.choice(self.values_corona[i])
-                    counter += 1
-            if userInput == 'q':
-                print(random.choice(self.values_corona[i]))
-                print("---------------------------------")
-                print("Do you want to choose another topic? Pick below or press 'q' to quit for realsies.")
-                print("---------------------------------")
-                break
-            resp = resp.format(word) # replace if applicable            
-            print(resp)
-
-    def meditating_Jordan(self):
-
-        """
-        
-        A function to meditate with Jordan as your guide.
-        
-        """
-        while True:
-            userInput = input(">>> ")
-            resp = "I did not understand what you said. Also, I am sensitive to punctuation." # initalize this
-            counter = 0  # initalize this
-            while resp == "I did not understand what you said. Also, I am sensitive to punctuation." and counter < len(self.keys_meditating):
-                for i in range(0, len(self.keys_meditating)):
-                    match = self.keys_meditating[i].match(userInput)
-                    if match:
-                        word = self.keys_caring[i].split(userInput)[1]  # We have to take the first element of this list                        
-                        resp = random.choice(self.values_meditating[i])
-                    counter += 1
-            if userInput == 'q':
-                print(random.choice(self.values_meditating[i]))
-                print("---------------------------------")
-                print("Do you want to choose another topic? Pick below or press 'q' to quit for realsies.")
-                print("---------------------------------")
-                break
-            resp = resp.format(word) # replace if applicable
-            print(resp)
+                if userInput == 'q':
+                    print(random.choice(self.values[i]))
+                    print("---------------------------------")
+                    print("Do you want to choose another topic? Pick below or press 'q' to quit for realsies.")
+                    print("---------------------------------")
+                    break
+                resp = resp.format(word)
+                print(resp) # print Jordan's answer
             
 
 # playsound("C:/Users/User/Downloads/Meditation.mp3")
+
+
+class execute_Jordan():
+    """
+    A class that lets you execute Jordan.'
+    ...
+    
+    Attributes 
+    ----------)
+    keys_smalltalk : list
+
+    Methods 
+    ----------
+    execute
+    """
+        
+    def execute(self, Jordan, chooseJordan):
+        """
+        
+        A method to execute Jordan.
+        
+        """
+        j = Jordan()
+        topics = ['smalltalk', 'caring', 'cursing', 'meditating', 'corona']
+        i = []
+        while True:
+            print("Welcome. My name is Jordan. You can now choose a topic that we can talk about.")
+            print("Press '1' to have some good old-fashioned smalltalk.")
+            print("Press '2' to tell me about your deepest sorrows and your destroyed soul.")
+            print("Press '3' to curse with me.")
+            print("Press '4' to talk about Corona.")
+            print("Press '5' for a quick meditation.")
+            print("Press 'q' to quit.")
+            # execute this welcome text and tell user what to press in order to pick a topic.
+            
+            choice = input(">>> ") # initialize input
+            
+            if choice == '1': # determine which key to press to initiate the specific topic
+                i = topics[0]
+                print("Alrighty, let's do some chitchatting.")# initiate welcome text for specific topic
+                print("Don't forget that I am sensitive to punctuation.")
+                j.chooseJordan() # initiate method
+            if choice == '2':
+                i = topics[1]
+                print("Please tell me about your deepest sorrows and your destroyed soul.")
+                print("Don't forget that I am sensitive to punctuation.")
+                j.chooseJordan()   
+            if choice == '3':
+                i = topics[2]
+                print("Make yourself ready. let's insult each other!")
+                print("Don't forget that I am sensitive to punctuation.")
+                j.chooseJordan()
+            if choice == '4':
+                i = topics[3]
+                print("Ok then, let's talk about Corona.")
+                print("Don't forget that I am sensitive to punctuation..")
+                j.chooseJordan()
+            if choice == '5':
+                i = topics[4]
+                print("I will be your guide in this meditation.")
+                print("Don't forget that I am sensitive to punctuation..")
+                j.chooseJordan()
+            if choice == 'q': # if user wants to quit
+                break
+            else: # if user pressed the wrong key.
+                print("Try again.")
+                
