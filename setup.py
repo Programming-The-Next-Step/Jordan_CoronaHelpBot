9fd38fc72eb14efa6a151cb727617c55f632cf07
MarlaDre <marla.a.dressel@gmail.com>
2020-05-29 11:29:44 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,17 +7,21 @@ Created on Tue May 19 15:20:51 2020
 
 import setuptools
 
-
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
 setuptools.setup(
-    name="Jordan.py",
+    name="jordan.py",
     version="0.0.1",
     py_modules=["jordan"],
     description="Chatbot helping you not get crazy in times of Corona.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="MarlaDre",
     author_email="marla.a.dressel@gmail.com",
     python_requires=">=3.6",
     url="https://github.com/Programming-The-Next-Step/Jordan_CoronaHelpBot",
-    packages="setuptools.find_packages()", # speciify dependencies
+    packages=setuptools.find_packages(), # speciify dependencies
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
