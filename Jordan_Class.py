635c78bb6421c5c511c6d02e3413a18d5066b161
MarlaDre <marla.a.dressel@gmail.com>
2020-05-30 12:03:08 +0200

    Update Jordan_Class.py

--- Jordan_Class.py
+++ Jordan_Class.py
@@ -55,107 +55,69 @@ class Jordan:
         """
         Constructing topics for Jordan. Takes no arguments.
         
-        """
-        self.library_keys = str('library' + self.i)
-        self.library_values = str('library' + self.i)
-        
-        self.keys = list(map(lambda x:re.compile(x[0], re.IGNORECASE), self.library_keys)) 
-            # list(map()) applies a function to all elements of a specified object, in this case the small-talk library
-            # lambda makes sure that re.compile is applied in a certain way to all elements of the library without being case-sensitive
-            # re.compile makes sure that the elemets are turned into objects that can be matched later to another item in the library
-        self.values = list(map(lambda x:x[1],self.library_values))
-            # same here, but here we pick the second argument in the list x[1], which entails Jordan's answers
-        
+        """        
     def chooseTopic(self):             
-        
-            while True:
-                userInput = input(">>> ") # this allows for the user to type in keys
-                resp = "I did not understand what you said. Also, I am sensitive to punctuation." # initalize response variable
-                counter = 0  # initalize counter
-                while resp == "I did not understand what you said. Also, I am sensitive to punctuation." and counter < len(self.keys): # determine when to start my loop
-                    for i in range(0, len(self.keys)): # loop through the indexed library
-                        match = self.keys[i].match(userInput) # look if input of the user matches with one of the words in the library
-                        if match:
-                            word = self.keys[i].split(userInput)[1]  # We have to take the first element of this list
-                            resp = random.choice(self.values)[i] # if there is a match, pick a random answer from x[1]
-                        counter += 1 # make sure that the counter is now + 1 so it does not write the initialized response from the beginning but continues with the loop
-                                     # if there is no match though, then it will write the initialized answer
-                
-                if userInput == 'q':
-                    print(random.choice(self.values[i]))
-                    print("---------------------------------")
-                    print("Do you want to choose another topic? Pick below or press 'q' to quit for realsies.")
-                    print("---------------------------------")
-                    break
-                resp = resp.format(word)
-                print(resp) # print Jordan's answer
-            
-
-# playsound("C:/Users/User/Downloads/Meditation.mp3")
-
-
-class execute_Jordan():
-    """
-    A class that lets you execute Jordan.'
-    ...
     
-    Attributes 
-    ----------)
-    keys_smalltalk : list
-
-    Methods 
-    ----------
-    execute
-    """
+        topics = [library_smalltalk, library_caring, library_cursing, library_meditating, library_corona]
         
-    def execute(self, Jordan, chooseJordan):
-        """
-        
-        A method to execute Jordan.
-        
-        """
-        j = Jordan()
-        topics = ['smalltalk', 'caring', 'cursing', 'meditating', 'corona']
-        i = []
         while True:
             print("Welcome. My name is Jordan. You can now choose a topic that we can talk about.")
-            print("Press '1' to have some good old-fashioned smalltalk.")
-            print("Press '2' to tell me about your deepest sorrows and your destroyed soul.")
-            print("Press '3' to curse with me.")
+            print("Press '0' to have some good old-fashioned smalltalk.")
+            print("Press '1' to tell me about your deepest sorrows and your destroyed soul.")
+            print("Press '2' to curse with me.")
+            print("Press '3' to talk about Corona.")
             print("Press '4' to talk about Corona.")
-            print("Press '5' for a quick meditation.")
-            print("Press 'q' to quit.")
             # execute this welcome text and tell user what to press in order to pick a topic.
             
-            choice = input(">>> ") # initialize input
+            choice = input(">>> ")
+            # initialize input
             
-            if choice == '1': # determine which key to press to initiate the specific topic
-                i = topics[0]
+            if choice == '0': # determine which key to press to initiate the specific topic
                 print("Alrighty, let's do some chitchatting.")# initiate welcome text for specific topic
                 print("Don't forget that I am sensitive to punctuation.")
-                j.chooseJordan() # initiate method
-            if choice == '2':
-                i = topics[1]
+            if choice == '1':
                 print("Please tell me about your deepest sorrows and your destroyed soul.")
-                print("Don't forget that I am sensitive to punctuation.")
-                j.chooseJordan()   
-            if choice == '3':
-                i = topics[2]
+                print("Don't forget that I am sensitive to punctuation.")  
+            if choice == '2':
                 print("Make yourself ready. let's insult each other!")
                 print("Don't forget that I am sensitive to punctuation.")
-                j.chooseJordan()
-            if choice == '4':
-                i = topics[3]
+            if choice == '3':
                 print("Ok then, let's talk about Corona.")
                 print("Don't forget that I am sensitive to punctuation..")
-                j.chooseJordan()
-            if choice == '5':
-                i = topics[4]
+            if choice == '4':
                 print("I will be your guide in this meditation.")
                 print("Don't forget that I am sensitive to punctuation..")
-                j.chooseJordan()
             if choice == 'q': # if user wants to quit
                 break
             else: # if user pressed the wrong key.
                 print("Try again.")
-                
+        
+            keys = list(map(lambda x:re.compile(x[0], re.IGNORECASE), topics[choice])) 
+            # list(map()) applies a function to all elements of a specified object, in this case the cursing library
+            # lambda makes sure that re.compile is applied in a certain way to all elements of the library without being case-sensitive
+            # re.compile makes sure that the elemets are turned into objects that can be matched later to another item in the library    
+            values = list(map(lambda x:x[1],topics[choice]))
+            # same here, but here we pick the second argument in the list x[1], which entails Jordan's answers
+        
+    
+        while True:
+            userInput = input(">>> ") # this allows for the user to type in keys
+            resp = "I did not understand what you said. Also, I am sensitive to punctuation." # initalize response variable
+            counter = 0  # initalize counter
+            while resp == "I did not understand what you said. Also, I am sensitive to punctuation." and counter < len(keys): # determine when to start my loop
+                for i in range(0, len(keys)): # loop through the indexed library
+                    match = keys[i].match(userInput) # look if input of the user matches with one of the words in the library
+                    if match:
+                        word = keys[i].split(userInput)[1]  # We have to take the first element of this list
+                        resp = random.choice(values)[i] # if there is a match, pick a random answer from x[1]
+                    counter += 1 # make sure that the counter is now + 1 so it does not write the initialized response from the beginning but continues with the loop
+                                 # if there is no match though, then it will write the initialized answer
+            
+            if userInput == 'q':
+                print(random.choice(values[i]))
+                print("---------------------------------")
+                print("Do you want to choose another topic? Pick below or press 'q' to quit for realsies.")
+                print("---------------------------------")
+                break
+            resp = resp.format(word)
+            print(resp) # print Jordan's answer
