d229b8accc6714f4262c2a08fdc51e2b9827f0f7
MarlaDre <marla.a.dressel@gmail.com>
2020-05-31 20:19:23 +0200

    Update __init__.py

--- unit testing/__init__.py
+++ unit testing/__init__.py
@@ -2,8 +2,15 @@
 """
 Created on Fri May 29 10:24:29 2020
 
-@author: User
+@author: Marla
 """
 
-from jordan.Jordan_Class import Jordan
+import re # import regular expressions for the appropriate access to the libraries
+import random # import random to pick a random answer for Jordan from the libraries
+from playsound import playsound
+from Jordan_Library import dict_smalltalk, dict_caring, dict_cursing, dict_meditating, dict_corona # import dictionairies
+from Jordan_Class import JordanBot
 
+test = JordanBot('Marla')
+
+test.chat()
