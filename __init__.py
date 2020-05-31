ee31940298aa1fa2c4d187417cca22886b52eab4
MarlaDre <marla.a.dressel@gmail.com>
2020-05-31 10:39:42 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,5 +5,8 @@ Created on Fri May 29 10:24:29 2020
 @author: User
 """
 
-from jordan.Jordan_Class import Jordan
+import re # import regular expressions for the appropriate access to the libraries
+import random # import random to pick a random answer for Jordan from the libraries
+from Jordan_Libraries import library_smalltalk, library_caring, library_cursing, library_corona, library_meditating # import libraries
 
+f
