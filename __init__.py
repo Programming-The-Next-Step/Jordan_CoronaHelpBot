f56501f75c63028ac7190d4829ea494a15a69f14
MarlaDre <marla.a.dressel@gmail.com>
2020-05-31 20:17:50 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,6 +7,4 @@ Created on Fri May 29 10:24:29 2020
 
 import re # import regular expressions for the appropriate access to the libraries
 import random # import random to pick a random answer for Jordan from the libraries
-from Jordan_Libraries import library_smalltalk, library_caring, library_cursing, library_corona, library_meditating # import libraries
-
-f
+from Jordan_Libraries import * # import libraries
