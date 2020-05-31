08d2bb5551cf6b1eb012fbebe9b2aabac18cd863
MarlaDre <marla.a.dressel@gmail.com>
2020-05-31 23:31:19 +0200

    Update Jordan_Execute.py

--- Jordan_Execute.py
+++ Jordan_Execute.py
@@ -5,10 +5,6 @@ Created on Sun May 31 14:47:15 2020
 @author: User
 """
 
-import re # import regular expressions for the appropriate access to the libraries
-import random # import random to pick a random answer for Jordan from the libraries
-from playsound import playsound
-from Jordan_Library import dict_smalltalk, dict_caring, dict_cursing, dict_meditating, dict_corona # import dictionairies
 from Jordan_Class import JordanBot
 
 test = JordanBot('Marla')
